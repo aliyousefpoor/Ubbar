a889963312fd3a4303e12b8ececac82d9022ecc4
AliYousefpoor <aliyousefpoor47@gmail.com>
2025-02-25 20:21:42 +0330

    Add viewmodel and fragments

---
+++ app/src/main/java/com/task/ubbar/presentation/UbbarAddressesFragment.kt
@@ -0,0 +1,8 @@
+package com.task.ubbar.presentation
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class UbbarAddressesFragment:Fragment() {
+}
---
+++ app/src/main/java/com/task/ubbar/presentation/UbbarViewModel.kt
@@ -0,0 +1,9 @@
+package com.task.ubbar.presentation
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class UbbarViewModel @Inject constructor() : ViewModel() {
+}
